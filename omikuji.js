d71908f1465c8daa9aa0e65105e94dccc2fa82cc
unknown <shotaro031005@gmail.com>
2022-12-18 11:10:46 +0900

    Initial Commit

--- omikuji.js
+++ omikuji.js
@@ -2,6 +2,9 @@ let username;
 let userresult;
 
 username = prompt("お名前を教えてください。");
+if (username == "") {
+  username = "名無し";
+}
 document.getElementById("name").innerHTML = username;
 
 let rand = Math.floor( Math.random() * 5) ;
